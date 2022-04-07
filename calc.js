77cbb91e99640ab41d0e8dc5ce22149f4c2cbef3
Noud van Dun <38325801+Noud63@users.noreply.github.com>
2022-04-07 11:23:04 +0200

    Add files via upload

--- calc.js
+++ calc.js
@@ -30,17 +30,7 @@ function calcTip() {
     rotateDollar()
 
     //Shows pop up with message when tip is 20% or more
-    let timer = setTimeout(() => {
-
-        youAreSoGenerous(serviceQual)
-
-        setTimeout(() => {
-            document.querySelector('.generous').style.visibility = 'hidden';
-            return () => {
-                clearTimeout(timer);
-            }
-        }, 4000);
-    }, 3000);
+    showMessage(serviceQual)
 
     //Calculate tip
     var totalTip = (billAmt * serviceQual) / numOfPeople;
@@ -55,13 +45,33 @@ function calcTip() {
 };
 
 
-// If tip is higher than 15% show popup message
+// If tip is 20%or more show popup message
 function youAreSoGenerous(serviceQual) {
     if (serviceQual >= '0.2') {
         document.querySelector('.generous').style.visibility = 'visible'
+        document.querySelector('.wow').style.visibility = 'visible'
     }
-    return
 };
+
+
+function showMessage(serviceQual) {
+    let timer = setTimeout(() => {
+        youAreSoGenerous(serviceQual)
+        document.querySelector('.wow').classList.toggle('wowRotate');
+
+        setTimeout(() => {
+            document.querySelector('.generous').style.visibility = 'hidden';
+        }, 6000);
+
+        setTimeout(() => {
+            document.querySelector('.wow').style.visibility = 'hidden'
+        }, 3000)
+        return ()=> {
+            clearTimeout(timer)
+        }
+
+    }, 4000);
+}
 
 
 //Rotate dollar sign on click
@@ -69,12 +79,11 @@ function rotateDollar() {
     document.querySelector('.topIcon').classList.toggle('topIconRotate');
     document.querySelector('.bottomIcon').classList.toggle('topIconRotate');
     document.querySelector('.bottomIcon2').classList.toggle('topIconRotate2');
-
     pushButton.disabled = true
 };
 
 
-// Hide each on page load and after clear input
+// Hide p.p. on page load and after clear input
 document.getElementById('each').style.display = 'none';
 
 
@@ -97,7 +106,6 @@ function deleteInput() {
     pushButton.disabled = false
 }
 
-deleteInput();
 
 
 
